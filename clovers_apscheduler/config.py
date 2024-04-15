3fd003c5997ef748be0c98ad8c6a929fdad54680
KarisAya <1048827424@qq.com>
2024-04-15 10:59:04 +0800

    移除其他分包

--- clovers_apscheduler/config.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Config(BaseModel):
-    scheduler_config = {"apscheduler.timezone": "Asia/Shanghai"}
--- test.py
+++
@@ -1,4 +0,0 @@
-print([0] * 4 + [1] * 2)
-from collections import Counter
-
-print(Counter([0] * 4 + [1] * 2))
