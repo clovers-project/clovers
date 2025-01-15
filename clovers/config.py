8d599292b56a57317163f99996ef9ea5e1a71460
KarisAya <1048827424@qq.com>
2025-01-15 12:46:04 +0800

    修改配置

--- clovers/config.py
+++ clovers/config.py
@@ -1,25 +1,34 @@
-import os
 import toml
+import os
 from pathlib import Path
+from functools import cache
+
+CONFIG_FILE = os.environ.get("CLOVERS_CONFIG_FILE", "clovers.toml")
 
 
 class Config(dict):
-    path: Path
-
     @classmethod
-    def load(cls, path):
-        path = Path(path)
+    def load(cls, path: str | Path = CONFIG_FILE):
+        print("加载中...")
+        path = Path(path) if isinstance(path, str) else path
         if path.exists():
             config = cls(toml.load(path))
         else:
             path.parent.mkdir(exist_ok=True, parents=True)
             config = cls()
-        config.path = path
         return config
 
-    def save(self):
-        with open(self.path, "w", encoding="utf8") as f:
+    def save(self, path: str | Path = CONFIG_FILE):
+        path = Path(path) if isinstance(path, str) else path
+        if not path.exists():
+            path.parent.mkdir(exist_ok=True, parents=True)
+        with open(path, "w", encoding="utf8") as f:
             toml.dump(self, f)
 
+    @classmethod
+    @cache
+    def environ(cls):
+        return cls.load(CONFIG_FILE)
 
-config = Config.load(os.environ.get("clovers_config_file", "clovers.toml"))
+
+config = Config.environ()
